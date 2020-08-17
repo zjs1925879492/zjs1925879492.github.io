4c9e5ea7e5b92ed4cceccf93f2d5e56017b8a707
Chang'anxiaoxiang <1925879492@qq.com>
2020-08-18 04:26:40 +0800

    music bug

--- everybed/music.js
+++ everybed/music.js
@@ -1,1 +1,1 @@
-var mp3='https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcjdQZmNWU3Y1aUpiODZ5VzN5TGpOcm13T2s/ZT1ENmE4YVg=.mp3';var mp3 = new Audio(mp3);mp3.play();setTimeout(window.location='zjs1925879492.github.io',3000)
+var mp3='https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcjdQZmNWU3Y1aUpiODZ5VzN5TGpOcm13T2s/ZT1ENmE4YVg=.mp3';var mp3 = new Audio(mp3);mp3.play();setTimeout(window.location='https://zjs1925879492.github.io',3000)
