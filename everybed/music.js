376b169fd26092d5bbd7ebac5a90fc35f5c2f004
Chang'anxiaoxiang <1925879492@qq.com>
2020-08-18 04:10:16 +0800

    update sns&music

--- everybed/music.js
+++ everybed/music.js
@@ -1,1 +1,1 @@
-var mp3='https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcjdQZmNWU3Y1aUpiODZ5VzN5TGpOcm13T2s/ZT1vc2RxOXQ=.mp3';var mp3 = new Audio(mp3);mp3.play()
+var mp3='/The Nights——Avicii.mp3';var mp3 = new Audio(mp3);mp3.play()
