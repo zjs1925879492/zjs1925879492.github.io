6517b96e15e0b96dd93bdd04d195a13429bfd5a7
Chang'anxiaoxiang <1925879492@qq.com>
2020-08-17 23:36:27 +0800

    oh fuck

--- everybed/music.js
+++ everybed/music.js
@@ -1,1 +1,1 @@
-var mp3='https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcjdQZmNWU3Y1aUpiODZ5VzN5TGpOcm13T2s/ZT1vc2RxOXQ=.mp3';var mp3 = new Audio(mp3);mp3.play();setTimeout('Redirect()', 3000);window.location.href="https://zjs1925879492.github.io"
+var mp3='https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcjdQZmNWU3Y1aUpiODZ5VzN5TGpOcm13T2s/ZT1vc2RxOXQ=.mp3';var mp3 = new Audio(mp3);mp3.play()
